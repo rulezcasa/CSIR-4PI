94e478c6c601bb8c2a065ce043fb60d476dcf000
Harish Thangaraj <108048779+rulezcasa@users.noreply.github.com>
2025-04-08 14:48:38 +0530

    Update vanilla_DQN.py

--- AT-DQN/Cartpole/vanilla_DQN.py
+++ AT-DQN/Cartpole/vanilla_DQN.py
@@ -4,14 +4,33 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from tqdm import tqdm
+import cv2
+import yaml
+from datetime import timedelta
+import pdb
+DEBUG = True
+if DEBUG:
+    import wandb
+    from wandb import AlertLevel
+
+# Initilizations
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+if config['Vanilla-DQN']['device']=='mps':
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+if config['Vanilla-DQN']['device']=='cuda':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.backends.cudnn.benchmark = True
 
 #defining the Q-network
 class QNetwork(nn.Module):
     def __init__(self, state_shape, action_shape):
         super(QNetwork, self).__init__()
-        self.fc1 = nn.Linear(state_shape, 128)  # cartpole input : (4,)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, action_shape)
+        self.fc1 = nn.Linear(*state_shape, 64)  # cartpole input : (4,)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_shape)
         self.relu = nn.ReLU()
 
     def forward(self, x):
@@ -22,8 +41,8 @@ class QNetwork(nn.Module):
 
 class ReplayBuffer:
     def __init__(
-        self, capacity, device="cuda"
-    ):  # adjusting replay buffer to 50K. ()
+        self, capacity, device=device
+    ):  
         self.capacity = capacity
         self.device = device
         self.device_cpu = "cpu"
@@ -85,12 +104,214 @@ class Agent:
     def __init__(self, state_space, action_space, lr):
         self.state_space=state_space
         self.action_space=action_space
-        self.qnetwork=QNetwork(self.state_space, self.action_space)
+        self.q_network = QNetwork(state_space, action_space).to(device)
+        self.target_network = QNetwork(state_space, action_space).to(device)
+        self.target_network.load_state_dict(self.q_network.state_dict())
         self.lr=lr
         self.optimizer = optim.Adam(self.q_network.parameters(), lr=self.lr)
-        self.replay_buffer=ReplayBuffer(100000, device="cuda")
-        self.gamma-0.99
+        self.replay_buffer=ReplayBuffer(config['Vanilla-DQN']['replay_buffer'], device=device)
+        self.gamma=config['Vanilla-DQN']['gamma']
+        self.exploration_count=0
+        self.exploitation_count=0
+        self.check_replay_size=config['Vanilla-DQN']['warmup'] #warmup steps
+        self.step_count=0
         
-        def act(self, state):
-            
+    def act(self, state, epsilon):
+        state_tensor = torch.from_numpy(state).float().unsqueeze(0).to(device)
 
+        self.q_network.eval()
+        with torch.no_grad():
+            action_values = self.q_network(state_tensor)
+        self.q_network.train()
+
+        if np.random.random() > epsilon: #exploit
+            self.exploitation_count+=1        	
+            return action_values.argmax(dim=1).item()
+        else:
+            self.exploration_count+=1 
+            return np.random.randint(self.action_space)
+            
+    def train_step(self):
+        if self.replay_buffer.size < self.check_replay_size:
+            return None
+        
+        states, actions, rewards, next_states, dones = self.replay_buffer.sample(128)
+        
+        # optimization - gpu
+        with torch.no_grad():
+            target_q_values = self.target_network(next_states)
+            max_next_q = target_q_values.max(dim=1, keepdim=True)[0]
+            targets = rewards + (1 - dones) * self.gamma * max_next_q
+
+        q_values = self.q_network(states).gather(1, actions.long())
+        td_errors = targets - q_values        
+        loss = torch.mean(td_errors**2)
+        self.optimizer.zero_grad()
+        loss.backward()
+        
+        # add grad clip
+        torch.nn.utils.clip_grad_norm_(self.q_network.parameters(), max_norm=10.0)
+        self.optimizer.step()
+        
+        self.step_count+=1
+        if self.step_count%config['Vanilla-DQN']['target_update']==0:
+            self.target_network.load_state_dict(self.q_network.state_dict())
+        return (
+            loss.item(),
+            td_errors.abs().squeeze().cpu().tolist(),
+            q_values.squeeze().cpu().tolist(),
+        )
+        
+    def add_experience(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(state, action, reward, next_state, done)        
+        
+def train_agent(env_name, render=False):
+    total_steps=config['Vanilla-DQN']['T']
+    epsilon_start=config['Vanilla-DQN']['epsilon_start']
+    epsilon_end=config['Vanilla-DQN']['epsilon_end']
+    decay_steps=config['Vanilla-DQN']['decay_steps']
+    lr=config['Vanilla-DQN']['lr']
+    epsilon=epsilon_start
+    
+    total_reward = 0
+    losses = []
+    episode = 0
+    episode_length = 0
+    td_errors_per_episode = []
+    q_values = []
+    
+    env = gym.make(env_name)
+    state, _ = env.reset()
+    state_shape = env.observation_space.shape
+    action_size = env.action_space.n
+    agent = Agent(state_shape, action_size, lr)
+    
+    
+    
+
+
+    for step in tqdm(range(total_steps), desc="Training Progress"):
+        episode_length += 1
+        if step < decay_steps:
+            epsilon = epsilon_start - (step / decay_steps) * (epsilon_start - epsilon_end)       
+        else:
+            epsilon = epsilon_end
+        action = agent.act(state,epsilon)
+        next_frame, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+        agent.add_experience(state, action, reward, next_frame, done)
+        result = agent.train_step()
+        state = next_frame
+        total_reward += reward
+        if result is not None:
+            loss, td_error, qvalue = result
+            losses.append(loss)
+            td_errors_per_episode.extend(td_error)
+            q_values.extend(qvalue)
+
+        if done:
+            episode += 1
+            mean_losses = np.mean(losses) if losses else 0.0
+            mean_td_error = (
+                np.mean(td_errors_per_episode) if td_errors_per_episode else 0.0
+            )
+            mean_q_value = np.mean(q_values) if q_values else 0.0
+
+            if DEBUG:
+                wandb.log(
+                    {
+                        "global_step": step + 1,
+                        "reward": total_reward,
+                        "loss": mean_losses,
+                        "episode_length": episode_length,
+                        "mean_td_error": mean_td_error,
+                        "mean_q_value": mean_q_value,
+                        "epsilon":epsilon,
+                    },
+                    step=episode,
+                )
+            #else:
+            #    print(
+            #        f"Episode {episode} - Steps: {step+1}, Reward: {total_reward:.2f}, Loss: {mean_losses:.4f}, "
+            #        f"Q Value: {mean_q_value:.4f}"
+            #    )
+
+
+            if episode % 100 == 0:
+                if DEBUG:
+                    wandb.log(
+                        {
+                            "explored_states": agent.exploration_count,
+                            "exploited_states": agent.exploitation_count,
+                        },
+                        step=episode,
+                    )
+                else:
+                    print(
+                        f"Episode {episode} - Explored: {agent.exploration_count}, Exploited: {agent.exploitation_count}"
+                    )
+                    print(
+                    f"Episode {episode} - Steps: {step+1}, Reward: {total_reward:.2f}, Loss: {mean_losses:.4f}, "
+                    f"Q Value: {mean_q_value:.4f}"
+                )
+
+
+                agent.exploration_count = 0
+                agent.exploitation_count = 0
+
+            state, _ = env.reset()
+            total_reward = 0
+            losses = []
+            episode_length = 0
+            td_errors_per_episode = []
+            q_values = []
+
+
+    print("Training complete!")
+    env.close()
+
+    os.makedirs("AT_DQN_Models", exist_ok=True)
+    torch.save(
+        agent.q_network.state_dict(), f"AT_DQN_Models/atdqn_{env_name.split('/')[-1]}_model.pth"
+    )
+    print(f"Model saved successfully!")
+
+    if DEBUG:
+        #wandb.save(model_path)
+        wandb.finish()
+    else:
+        print("Debug mode disabled, skipping wandb model upload")
+
+
+if __name__ == "__main__":
+    if DEBUG:
+        wandb.init(
+            project="AT-DQN",
+            name="cartpole_vanilla_v4",
+            config={
+                "total_steps": config['Vanilla-DQN']['T'],
+                "epsilon_start": config['Vanilla-DQN']['epsilon_start'],
+                "epsilon_end": config['Vanilla-DQN']['epsilon_end'],
+                "lr": config['Vanilla-DQN']["lr"],
+            },
+        )
+    else:
+        print("Running in non-debug mode, wandb logging disabled")
+        print(
+            f"Config: {config['Vanilla-DQN']['T']}, {config['Vanilla-DQN']['epsilon_start']}, beta_end={config['Vanilla-DQN']['epsilon_end']}, lr={config['Vanilla-DQN']['lr']}"
+        )
+
+    # Train agent
+    train_agent("CartPole-v1")
+        
+        
+        
+    
+
+ 
+        
+        
+	    
+           	
+        
+            
